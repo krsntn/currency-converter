a14bfb24551969bc44ae6b41dca26fbfd6cb6834
krsntn <kanahan0110@gmail.com>
2023-08-10 16:10:26 +0800

    pwa

--- vite.config.js
+++ vite.config.js
@@ -12,19 +12,31 @@ export default defineConfig({
       registerType: "autoUpdate",
       manifest: {
         name: "Currency Converter",
-        short_name: "CC",
+        short_name: "Currency Converter",
         description: "Currency Converter App",
         theme_color: "#000000",
         icons: [
           {
-            src: "src/assets/192.png",
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "src/assets/512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
